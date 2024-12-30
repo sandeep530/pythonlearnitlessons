f322901121333a69cab5c473b97e722148f81721
MOTHE SANDEEP <sandeepmothe824@gmail.com>
2024-12-30 13:22:58 +0530

    forth commit

---
+++ 4. multiple-print.py
@@ -0,0 +1,10 @@
+print("hello world",)
+print("welcome to world",)
+print("welcome back to india")
+
+print("rose and flowers,\nred and flowers,\nyellow and flowers")
+
+print(""""rose and flowers,
+red and flowers,
+yellow and flowers""")
+
