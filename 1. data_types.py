afb0b2d93665bcde4d9e5f28b85df1fc6927fccf
MOTHE SANDEEP <sandeepmothe824@gmail.com>
2025-01-02 10:03:40 +0530

    fifth commit

---
+++ 1. data_types.py
@@ -0,0 +1,12 @@
+name = 'sandeep'
+age = 27
+height_meter = 5.5
+is_student = False
+
+
+
+print('name:', name) #string
+print('age:', age) #init
+print('height_meter'
+      ':', height_meter) #floats
+print('Are you a student?', is_student)
--- 4. multiple-print.py
+++ 4. multiple-print.py
@@ -1,10 +1,17 @@
 print("hello world",)
 print("welcome to world",)
 print("welcome back to india")
-
 print("rose and flowers,\nred and flowers,\nyellow and flowers")
-
 print(""""rose and flowers,
 red and flowers,
 yellow and flowers""")
 
+# ths is python multiple script
+
+
+x =10
+y = 20
+
+result = x + y
+
+print(result)
